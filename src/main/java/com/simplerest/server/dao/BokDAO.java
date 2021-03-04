159714c43edf32da1550268666c561ffcf0c6a89
tdl1304 <tommy.1304@hotmail.com>
2021-03-04 23:00:42 +0100

    Finished DAO classes

--- src/main/java/com/simplerest/server/dao/BokDAO.java
+++ src/main/java/com/simplerest/server/dao/BokDAO.java
@@ -1,34 +1,62 @@
 package com.simplerest.server.dao;
 
+import com.simplerest.server.entities.Adresse;
 import com.simplerest.server.entities.Bok;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 import java.util.List;
 
 @Repository
 public class BokDAO implements Dao<Bok>{
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
     @Override
     public void save(Bok bok) {
-
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(bok);
+        em.getTransaction().commit();
+        em.close();
     }
 
     @Override
     public Bok load(int id) {
-        return null;
+        EntityManager em = emf.createEntityManager();
+        Bok bok = em.find(Bok.class, id);
+        em.close();
+        return bok;
     }
 
     @Override
     public void delete(int id) {
+        EntityManager em = emf.createEntityManager();
+        Bok bok = em.find(Bok.class, id);
 
+        em.getTransaction().begin();
+        em.remove(bok);
+        em.getTransaction().commit();
+        em.close();
     }
 
     @Override
     public void update(Bok bok) {
-
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.merge(bok);
+        em.getTransaction().commit();
+        em.close();
     }
 
     @Override
     public List<Bok> loadAll() {
-        return null;
+        EntityManager em = emf.createEntityManager();
+        List<Bok> boker = em.createQuery("SELECT a FROM Bok a")
+                .getResultList();
+        em.close();
+        return boker;
     }
 }
--- src/main/java/com/simplerest/server/dao/ForfatterDAO.java
+++ src/main/java/com/simplerest/server/dao/ForfatterDAO.java
@@ -3,32 +3,59 @@ package com.simplerest.server.dao;
 import com.simplerest.server.entities.Forfatter;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
 import java.util.List;
 
 @Repository
 public class ForfatterDAO implements Dao<Forfatter>{
+    @PersistenceUnit
+    EntityManagerFactory emf;
+
     @Override
     public void save(Forfatter forfatter) {
-
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(forfatter);
+        em.getTransaction().commit();
+        em.close();
     }
 
     @Override
     public Forfatter load(int id) {
-        return null;
+        EntityManager em = emf.createEntityManager();
+        Forfatter forfatter = em.find(Forfatter.class, id);
+        em.close();
+        return forfatter;
     }
 
     @Override
     public void delete(int id) {
+        EntityManager em = emf.createEntityManager();
+        Forfatter forfatter = em.find(Forfatter.class, id);
 
+        em.getTransaction().begin();
+        em.remove(forfatter);
+        em.getTransaction().commit();
+        em.close();
     }
 
     @Override
     public void update(Forfatter forfatter) {
-
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+        em.merge(forfatter);
+        em.getTransaction().commit();
+        em.close();
     }
 
     @Override
     public List<Forfatter> loadAll() {
-        return null;
+        EntityManager em = emf.createEntityManager();
+        List<Forfatter> forfatters = em.createQuery("SELECT a FROM Forfatter a")
+                .getResultList();
+        em.close();
+        return forfatters;
     }
 }
